09e825c05cd23eb2c23e1ae5fd2011a695a916cb
JieCheng <ninthmelody@163.com>
2022-09-20 18:40:48 +0800

    feat: add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import countStore from './countStore';
 
 function MyButton() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count] = countStore.useState('value');
 
   function handleClick() {
-    setCount(count + 1);
+    countStore.setState({ value: count + 1 });
   }
 
   return <button onClick={handleClick}>Clicked {count} times</button>;
@@ -13,6 +15,7 @@ function MyButton() {
 function App() {
   return (
     <div>
+      <MyButton />
       <MyButton />
     </div>
   );
---
+++ src/countStore.ts
@@ -0,0 +1,7 @@
+import Store from './store';
+
+const countStore = new Store({
+  value: 0,
+});
+
+export default countStore;
